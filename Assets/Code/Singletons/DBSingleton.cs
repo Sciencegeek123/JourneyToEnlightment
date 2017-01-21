4f3e4f5e00be044056cd91d6b39c2ebf4843439d
Ryan Ngo <rawrimryno@gmail.com>
2017-01-20 23:00:48 -0600

    database!

--- Assets/Code/Singletons/DBSingleton.cs
+++ Assets/Code/Singletons/DBSingleton.cs
@@ -4,13 +4,132 @@ using Mono.Data.Sqlite;
 using System.Data;
 using UnityEngine;
 
-public class DBSingleton : MonoBehaviour {
+public class DBSingleton {
 
-    public static DBSingleton db;
-    public static IDbConnection dbconn;
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static DBSingleton dbinstance;
+    private static IDbConnection conn = null;
+    private static IDbCommand cmd = null;
+    private string connString = "URI=file:" + Application.dataPath + "/Code/Data/JourneyToEnlightenment.db";
+    private static StateSingleton ss;
+
+    public static DBSingleton get()
+    {
+        if (dbinstance == null)
+        {
+            dbinstance = new DBSingleton();
+        }
+        return dbinstance;
+    }
+
+    public IDbConnection getConn()
+    {
+        if (conn == null)
+        {
+            Debug.Log("Creating Connection...");
+            conn = (IDbConnection)new SqliteConnection(connString);
+            conn.Open();
+            if (conn == null)
+            {
+                Debug.LogError("ERROR: Database not found.");
+            }
+            return conn;
+        }else if (conn.State == ConnectionState.Closed || conn.State == ConnectionState.Broken)
+        {
+            Debug.Log("Recreating Connection...");
+            conn = (IDbConnection)new SqliteConnection(connString);
+            conn.Open();
+            if (conn == null)
+            {
+                Debug.LogError("ERROR: Database not found.");
+            }
+            return conn;
+        }else if (conn.State == ConnectionState.Open)
+        {
+            return conn;
+        }else
+        {
+            Debug.LogError("Trying to execute new query before one has finished");
+            return null;
+        }
+    }
+    public void closeConn()
+    {
+        if (conn.State == ConnectionState.Open)
+        {
+            Debug.Log("Closing Connection...");
+            conn.Close();
+            conn = null;
+        }
+    }
+
+    public IDbCommand getCmd()
+    {
+        if (conn == null)
+        {
+            Debug.LogError("ERROR: No Connection Esdtablished");
+            return null;
+        }else if (cmd == null)
+        {
+            cmd = conn.CreateCommand();
+        }else if (cmd.CommandText != null)
+        {
+            cmd.Dispose();
+            cmd = conn.CreateCommand();
+        }
+        return cmd;
+    }
+
+    public void closeCommand()
+    {
+        if (cmd != null)
+        {
+            cmd.Dispose();
+            cmd = null;
+        }
+    }
+
+    public int createNewPlayer(string name)
+    {
+        conn = getConn();
+        cmd = getCmd();
+        Debug.Log("Creating User...");
+        cmd.CommandText = "INSERT INTO Player (Name) Values (\"" + name + "\");";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "SELECT UID FROM Player ORDER BY UID DESC";
+        IDataReader reader = cmd.ExecuteReader();
+        reader.Read();
+        int id = reader.GetInt32(0);
+        reader.Close();
+        Debug.Log("User " + name + " has been created with user id: " + id);
+        return id;
+    }
+
+    public int deletePlayer(int uid) {
+        conn = getConn();
+        cmd = getCmd();
+        Debug.Log("Deleting User...");
+        cmd.CommandText = "Delete FROM Player Where UID = " + uid;
+        int result = cmd.ExecuteNonQuery();
+        if (result > 0)
+            return result;
+        else
+        {
+            Debug.LogError("User could not be deleted");
+            return -1;
+        }
+    }    
+
+    public void setBells()
+    {
+        ss = StateSingleton.get();
+        conn = getConn();
+        cmd = getCmd();
+        cmd.CommandText = "SELECT bell1,bell2,bell3,bell4,bell5,bell6 from Player where UID = " + ss.uid;
+        cmd.ExecuteReader();
+        IDataReader reader = cmd.ExecuteReader();
+        reader.Read();
+        int[] bells = [reader.GetInt32(0), reader.GetInt32(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetInt32(4), reader.GetInt32(5)];
+        ss.setBells(bells);
+        Debug.Log("Loaded Bells " + bells.ToString() + " to Player " + ss.uid);
+    }
 }
---
+++ Assets/Code/Singletons/StateSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateSingleton : MonoBehaviour {
+
+    private static StateSingleton instance;
+    private static DBSingleton db;
+    public int uid { get; set; }
+    public int[] bells = [0, 0, 0, 0, 0, 0];
+
+    public static StateSingleton get()
+    {
+        if (instance == null)
+        {
+            instance = new StateSingleton();
+        }
+        return instance;
+    }
+
+	// Use this for initialization
+	void Start () {
+        db = DBSingleton.get();
+        Debug.Log(db.createNewPlayer("User 1"));
+        Debug.Log(db.deletePlayer(6));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setBells(int[] bellsArr)
+    {
+        for (int i = 0; i < bellsArr.Length; i++)
+        {
+            bellsArr[i] = bellsArr[i];
+        }
+    }
+}

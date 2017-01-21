4a62c402d80163eea07990b87da2c8e60e10e881
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-21 01:49:02 -0600

    Shader forge and stuff.

---
+++ Assets/Code/Singletons/InputSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSingleton {
+
+	private static InputSingleton instance = null;
+
+	enum KeyEvents {
+		MoveForward,
+		MoveBackwards,
+		MoveLeft,
+		MoveRight,
+		TurnLeft,
+		TurnRight,
+		BellFire,
+		BellLeft,
+		BellRight,
+		Pause
+	}
+
+	public static InputSingleton Instance { 
+		get {
+			if(instance == null) {
+				instance = new InputSingleton();
+			} 
+			return instance;
+		}
+	}
+
+	public void Update() {
+		
+	}
+}

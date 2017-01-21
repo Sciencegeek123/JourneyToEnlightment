1f1d74d30cc13d29c57e6ece57768aae5c214589
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-21 04:26:30 -0600

    Working on lighting for bells

---
+++ Assets/Code/Testing/LightBellTestingScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBellTestingScript : MonoBehaviour {
+
+	[SerializeField] Light mySpotLight;
+	[SerializeField] Light myPointLight;
+
+	[SerializeField] AnimationCurve SpotLightIntensityCurve;
+	[SerializeField] AnimationCurve PointLightIntensityCurve;
+	[SerializeField] AnimationCurve SpotLightAngleCurve;
+	[SerializeField] float SpotLightDuration = 5;
+	private IEnumerator myLightCoRoutine = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			if(myLightCoRoutine != null) {
+				StopCoroutine(myLightCoRoutine);
+			}
+			myLightCoRoutine = PlayLightCoRoutine();
+			StartCoroutine(myLightCoRoutine);
+		}
+		
+	}
+
+	IEnumerator PlayLightCoRoutine() {
+		float startTime = Time.realtimeSinceStartup;
+		
+		while(Time.realtimeSinceStartup - startTime < SpotLightDuration) {
+			float delta = (Time.realtimeSinceStartup - startTime) / SpotLightDuration;
+			float spotAngle = Mathf.Clamp01(SpotLightAngleCurve.Evaluate(delta))*180;
+			float spotIntensity = Mathf.Clamp01(SpotLightIntensityCurve.Evaluate(delta));
+			float pointIntensity = Mathf.Clamp01(SpotLightIntensityCurve.Evaluate(delta))*7+1;
+
+			mySpotLight.intensity = spotIntensity;
+			mySpotLight.spotAngle = spotAngle;
+			myPointLight.intensity = pointIntensity;
+
+			yield return null;
+		}
+	}
+}

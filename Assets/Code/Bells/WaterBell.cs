22b81477112c287f16e3ee1fd34a4da393f63abb
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-22 13:36:58 -0600

    Water

--- Assets/Code/Bells/WaterBell.cs
+++ Assets/Code/Bells/WaterBell.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 
 public class WaterBell : BaseBell
 {
+	[SerializeField] GameObject WaterParticlePrefab;
     public override void Emit(float range)
     {
         Debug.Log("Emitting Water");
         //BellEventEmitterSingleton.Instance.Emit(BellEventType.WaterBellEvent, GetComponentInParent<Transform>(), range);
+    
+        //BellEventEmitterSingleton.Instance.Emit(BellEventType.AwarenessBellEvent, this.transform, range);
+		Instantiate(WaterParticlePrefab,this.transform.position, this.transform.rotation,null);
+
+        foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.AwarenessBellEvent])
+        {
+            if (item.p != null && (item.o.position - transform.position).magnitude < range)
+            {
+                item.p(BellEventType.AwarenessBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
+            }
+        }
+        foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.AllBellEvents])
+        {
+            if (item.p != null && (item.o.position - transform.position).magnitude < range)
+            {
+                item.p(BellEventType.AwarenessBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
+            }
+        }
     }
+    
 }

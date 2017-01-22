b268261471b54467d19979b09b979b9bbcc1ac1d
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-22 05:08:41 -0600

    Fixed lighting

--- Assets/Code/Bells/EnlightenmentBell.cs
+++ Assets/Code/Bells/EnlightenmentBell.cs
@@ -8,19 +8,5 @@ public class EnlightenmentBell : BaseBell
     public override void Emit(float range)
     {
         Debug.Log("Emitting Enlightenment");
-        foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.EnlightenmentBellEvent])
-        {
-            if (item.p != null && (item.o.position - transform.position).magnitude < range)
-            {
-                item.p(BellEventType.EnlightenmentBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
-            }
-        }
-        foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.AllBellEvents])
-        {
-            if (item.p != null && (item.o.position - transform.position).magnitude < range)
-            {
-                item.p(BellEventType.EnlightenmentBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
-            }
-        }
     }
 }

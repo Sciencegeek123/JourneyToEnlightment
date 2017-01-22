7c6446a3df49458b1c29620a0c4bdd718b79fbda
Ryan Ngo <rawrimryno@gmail.com>
2017-01-22 02:15:05 -0600

    no message

--- Assets/Code/Mobs/BatAudioScript.cs
+++ Assets/Code/Mobs/BatAudioScript.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrogAudioScript : MonoBehaviour {
+public class BatAudioScript : MonoBehaviour
+{
     AudioSource audioSource;
-    public AudioClip[] FrogCroaks;
+    public AudioClip[] BatNoises;
     Player player;
     public float croakChance = 0.998f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         audioSource = GetComponent<AudioSource>();
         player = FindObjectOfType<Player>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (audioSource != null)
         {
             if (audioSource.isPlaying == false
                 && Random.Range(0.0f, 1.0f) > croakChance
-                && FrogCroaks.Length > 0)
+                && BatNoises.Length > 0)
             {
-                audioSource.clip = FrogCroaks[Random.Range(0, FrogCroaks.Length - 1)];
+                audioSource.clip = BatNoises[Random.Range(0, BatNoises.Length - 1)];
                 //Debug.Log("Croak!");
                 audioSource.Play();
             }

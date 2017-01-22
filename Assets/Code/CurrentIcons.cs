c8bfb4b3d4c18dceb847ba404c2561a829cb7cc9
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-22 14:02:49 -0600

    All

---
+++ Assets/Code/CurrentIcons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentIcons : MonoBehaviour {
+
+
+	[SerializeField] Image iconLocation;
+	[SerializeField] Sprite[] Icons;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		StateSingleton instance = StateSingleton.get();
+
+		if(instance!=null) {
+			iconLocation.sprite = Icons[instance.curBell]; 
+		}
+	}
+}

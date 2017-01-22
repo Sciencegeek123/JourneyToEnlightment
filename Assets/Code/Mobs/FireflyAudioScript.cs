768cd85c5f9cf51416df8d28cf05b49bf5ad5172
Ryan Ngo <rawrimryno@gmail.com>
2017-01-22 02:31:51 -0600

    no message

---
+++ Assets/Code/Mobs/FireflyAudioScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyAudioScript : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip[] fireflyNoises;
+    Player player;
+    public float croakChance = 0.998f;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audioSource != null)
+        {
+            if (audioSource.isPlaying == false
+                && Random.Range(0.0f, 1.0f) > croakChance
+                && fireflyNoises.Length > 0)
+            {
+                audioSource.clip = fireflyNoises[Random.Range(0, fireflyNoises.Length - 1)];
+                //Debug.Log("Croak!");
+                audioSource.Play();
+            }
+        }
+    }
+}

8f3493e5de42043de3d18a138fec1e924d722000
Ryan Ngo <rawrimryno@gmail.com>
2017-01-22 01:44:19 -0600

    no message

--- Assets/Code/Effects/EffectsCheats.cs
+++ Assets/Code/Effects/EffectsCheats.cs
@@ -16,32 +16,32 @@ public class EffectsCheats : MonoBehaviour {
         if (Input.GetKeyDown("f1"))
         {
             Debug.Log("Emitting Awareness...");
-            bees.Emit(BellEventType.AwarenessBellEvent, transform, 1000f);
+            bees.Emit(BellEventType.AwarenessBellEvent, transform, 5f);
         }
         if (Input.GetKeyDown("f2"))
         {
             Debug.Log("Emitting Air...");
-            bees.Emit(BellEventType.AirBellEvent, transform, 1000f);
+            bees.Emit(BellEventType.AirBellEvent, transform, 5f);
         }
         if (Input.GetKeyDown("f3"))
         {
             Debug.Log("Emitting Fire...");
-            bees.Emit(BellEventType.FireBellEvent, transform, 1000f);
+            bees.Emit(BellEventType.FireBellEvent, transform, 5f);
         }
         if (Input.GetKeyDown("f4"))
         {
             Debug.Log("Emitting Water...");
-            bees.Emit(BellEventType.WaterBellEvent, transform, 1000f);
+            bees.Emit(BellEventType.WaterBellEvent, transform, 5f);
         }
         if (Input.GetKeyDown("f5"))
         {
             Debug.Log("Emitting Earth...");
-            bees.Emit(BellEventType.EarthBellEvent, transform, 1000f);
+            bees.Emit(BellEventType.EarthBellEvent, transform, 5f);
         }
         if (Input.GetKeyDown("f6"))
         {
             Debug.Log("Emitting Everything...");
-            bees.Emit(BellEventType.AllBellEvents, transform, 1000f);
+            bees.Emit(BellEventType.AllBellEvents, transform, 5f);
         }
     }
 }

8edd7052925996252a8a6b5bac58f0d7997bd94f
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-22 04:08:38 -0600

    Wind particles

--- Assets/Code/Bells/AirBell.cs
+++ Assets/Code/Bells/AirBell.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 
 public class AirBell : BaseBell
 {
+    [SerializeField] GameObject WindProjectile;
+
+    float startTime;
+    int emittedParticles = 4;
     public override void Emit(float range)
     {
         Debug.Log("Emitting Air");
         BellEventEmitterSingleton.Instance.Emit(BellEventType.AirBellEvent, GetComponentInParent<Transform>(), range);
+        emittedParticles = 0;
+    }
+
+    void Update() {
+        if(emittedParticles < 4 && Time.realtimeSinceStartup - startTime > 0.25f) {
+            startTime = Time.realtimeSinceStartup;
+            emittedParticles++;
+            Instantiate(WindProjectile, transform.position, transform.rotation);
+        }
     }
 }

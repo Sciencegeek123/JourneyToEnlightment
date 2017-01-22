dbc76f20733f5309eacc4c39b59a04b462b169f1
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-22 11:48:07 -0600

    Fog fixer

--- Assets/Code/Effects/AwarenessParticles.cs
+++ Assets/Code/Effects/AwarenessParticles.cs
@@ -8,7 +8,8 @@ public class AwarenessParticles : MonoBehaviour {
 	[SerializeField] AnimationCurve SpotLightIntensityCurve;
 	[SerializeField] AnimationCurve PointLightIntensityCurve;
 	[SerializeField] AnimationCurve SpotLightAngleCurve;
-	[SerializeField] AnimationCurve FogDensityCurve;
+	[SerializeField] AnimationCurve FogStartCurve;
+	[SerializeField] AnimationCurve FogEndCurve;
 	[SerializeField] float SpotLightDuration = 5;
 
     void Start() {
@@ -25,7 +26,8 @@ public class AwarenessParticles : MonoBehaviour {
 			float spotAngle = Mathf.Clamp01(SpotLightAngleCurve.Evaluate(delta))*180;
 			float spotIntensity = SpotLightIntensityCurve.Evaluate(delta);
 			float pointIntensity = PointLightIntensityCurve.Evaluate(delta);
-            RenderSettings.fogDensity = FogDensityCurve.Evaluate(delta);
+            RenderSettings.fogStartDistance = FogStartCurve.Evaluate(delta);
+            RenderSettings.fogEndDistance = FogEndCurve.Evaluate(delta);
 
 			mySpotLight.intensity = spotIntensity;
 			mySpotLight.spotAngle = spotAngle;

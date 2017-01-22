78005ae43fce56025dd304af5d7d39c048c1db8f
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-22 13:08:57 -0600

    Stuff

--- Assets/Code/LoadSequence.cs
+++ Assets/Code/LoadSequence.cs
@@ -6,42 +6,123 @@ using UnityEngine.SceneManagement;
 
 public class LoadSequence : MonoBehaviour {
 
-	[SerializeField] float LoadDelay = 10;
-	[SerializeField] float LoadDelay2 = 15;
-	[SerializeField] float KillDelay = 20;
+	[SerializeField] float CloseupTime = 10;
+	[SerializeField] float TransitionLength = 5;
+	[SerializeField] float TitleFadeTime = 1;
+	[SerializeField] Camera myCamera;
+	[SerializeField] Gradient FadeInGradient;
+	[SerializeField] Gradient FadeOutGradient;
+	[SerializeField] Gradient TitleFadeInGradient;
+	[SerializeField] Image Background;
+	[SerializeField] Image Title;
 
-	bool loadOnce = false;
-	bool loadTwice = false;
-	[SerializeField] Gradient PaneAlpha;
-	[SerializeField] Image image;
-
-	// Update is called once per frame
-	void Update () {
-		image.color = PaneAlpha.Evaluate(Time.realtimeSinceStartup / 20);
-		if(!loadOnce && Time.realtimeSinceStartup > LoadDelay) {
-			SceneManager.LoadSceneAsync("MasterScene",LoadSceneMode.Additive);
-			loadOnce = true;
-		}
-		if(!loadTwice && Time.realtimeSinceStartup > LoadDelay2) {
-			LoadAll();
-			loadTwice = true;
-		}
-		if(Time.realtimeSinceStartup > KillDelay) {
-			SceneManager.UnloadSceneAsync("Intro");
-			this.gameObject.SetActive(false);
-		}
+	void Awake() {
+		StartCoroutine(LoadScenesCoroutine());
 	}
 
-	void LoadAll() {
-		SceneManager.LoadSceneAsync("MainGameEnvironment",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("AmbientSourceScene",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("EnemyScene",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("Starting Decoration",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("WallColliders",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("WallScene1",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("WallScene2",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("WallScene3",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("WallScene4",LoadSceneMode.Additive);
-		SceneManager.LoadSceneAsync("WallScene5",LoadSceneMode.Additive);
+	IEnumerator LoadScenesCoroutine() {
+		float startTime = Time.realtimeSinceStartup;
+		while(Time.realtimeSinceStartup - startTime < TransitionLength) {
+			float delta = (Time.realtimeSinceStartup - startTime) / TransitionLength;
+			Background.color = FadeInGradient.Evaluate(delta);
+			yield return null;
+		}
+
+		startTime = Time.realtimeSinceStartup;
+		while(Time.realtimeSinceStartup - startTime < CloseupTime) {
+			yield return null;
+		}
+
+		startTime = Time.realtimeSinceStartup;
+		while(Time.realtimeSinceStartup - startTime < TransitionLength) {
+			float delta = (Time.realtimeSinceStartup - startTime) / TransitionLength;
+			Background.color = FadeOutGradient.Evaluate(delta);
+			yield return null;
+		}
+
+		startTime = Time.realtimeSinceStartup;
+		while(Time.realtimeSinceStartup - startTime < TitleFadeTime) {
+			float delta = (Time.realtimeSinceStartup - startTime) / TitleFadeTime;
+			Title.color = TitleFadeInGradient.Evaluate(delta);
+			yield return null;
+		}
+
+		startTime = Time.realtimeSinceStartup;
+
+		AsyncOperation status;
+		{
+			status = SceneManager.LoadSceneAsync("MasterScene",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("MainGameEnvironment",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("WallScene1",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("WallScene2",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("WallScene3",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("WallScene4",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("WallScene5",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("Starting Decoration",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("WallColliders",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("AmbientSourceScene",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+		{
+			 status = SceneManager.LoadSceneAsync("EnemyScene",LoadSceneMode.Additive);
+			while(!status.isDone) {
+				yield return null;
+			}
+		}
+
+		while(Time.realtimeSinceStartup - startTime < CloseupTime) {
+			yield return null;
+		}
+
+		myCamera.gameObject.SetActive(false);
+		 SceneManager.UnloadSceneAsync("Intro");
+
 	}
 }

9d5c8b24d17cb28a0d793d0526bbda47e1bd0ee4
DESKTOP-KVJNAPG\toddm <toddmoulder88@gmail.com>
2017-01-22 14:02:23 -0600

    what happened to my fucking audio mixer

--- Assets/Code/Bells/WaterBell.cs
+++ Assets/Code/Bells/WaterBell.cs
@@ -5,30 +5,28 @@ using UnityEngine;
 
 public class WaterBell : BaseBell
 {
+	[SerializeField] GameObject WaterParticlePrefab;
     public override void Emit(float range)
     {
         Debug.Log("Emitting Water");
         //BellEventEmitterSingleton.Instance.Emit(BellEventType.WaterBellEvent, GetComponentInParent<Transform>(), range);
-<<<<<<< HEAD
     
-        //BellEventEmitterSingleton.Instance.Emit(BellEventType.AwarenessBellEvent, this.transform, range);
 		Instantiate(WaterParticlePrefab,this.transform.position, this.transform.rotation,null);
 
         foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.AwarenessBellEvent])
         {
-            if (item.p != null && item.o != null && (item.o.position - transform.position).magnitude < range)
+            if (item.p != null && (item.o.position - transform.position).magnitude < range)
             {
                 item.p(BellEventType.AwarenessBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
             }
         }
         foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.AllBellEvents])
         {
-            if (item.p != null && item.o != null && (item.o.position - transform.position).magnitude < range)
+            if (item.p != null && (item.o.position - transform.position).magnitude < range)
             {
                 item.p(BellEventType.AwarenessBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
             }
         }
-=======
->>>>>>> 9ffff481e8cab0c4c0e3d00a32fccbeb5ddaa76a
     }
+    
 }

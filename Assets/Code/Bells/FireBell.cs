7f8277368a96fbd7fcc2b08a3ab1b334948d547d
W. Craig Jones <wcjones@afterhourslab.com>
2017-01-22 11:39:38 -0600

    Fixed walls

--- Assets/Code/Bells/FireBell.cs
+++ Assets/Code/Bells/FireBell.cs
@@ -15,5 +15,34 @@ public class FireBell : BaseBell
         {
             item.Play();
         }
+        
+        foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.FireBellEvent])
+        {
+            if (item.p != null)
+            {
+                if ((item.o.position - transform.position).magnitude < range)
+                {
+                    float distanceFromLine = Vector3.Cross(transform.rotation * Vector3.forward, item.o.position - transform.position).magnitude;
+                    Debug.Log("Line distance: " + distanceFromLine);
+                    if (distanceFromLine < 5)
+                    {
+                        item.p(BellEventType.FireBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
+                    }
+                }
+            }
+        }
+        foreach (var item in BellEventEmitterSingleton.Instance.PingListeners[(int)BellEventType.AllBellEvents])
+        {
+            if (item.p != null)
+            {
+                if ((item.o.position - transform.position).magnitude < range)
+                {
+                    if (Vector3.Cross(transform.rotation * Vector3.forward, item.o.position - transform.position).magnitude < 5)
+                    {
+                        item.p(BellEventType.FireBellEvent, transform, Mathf.Sqrt((item.o.position - transform.position).magnitude) + 2.5f);
+                    }
+                }
+            }
+        }
     }
 }

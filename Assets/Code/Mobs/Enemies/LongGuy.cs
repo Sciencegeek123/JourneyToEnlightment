716d96b76e1ee6627512939c668d28065b85158d
DESKTOP-KVJNAPG\toddm <toddmoulder88@gmail.com>
2017-01-25 12:12:33 -0600

    proper longguy responses to bells

--- Assets/Code/Mobs/Enemies/LongGuy.cs
+++ Assets/Code/Mobs/Enemies/LongGuy.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LongGuy : BaseEnemy {
+public class LongGuy : BaseEnemy
+{
 
-	// Use this for initialization
-	public override void Start () {
+    // Use this for initialization
+    public override void Start()
+    {
         base.Start();
-	}
-	
-	// Update is called once per frame
-	public override void Update () {
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
         base.Update();
-	}
+    }
+    public override void Ping(BellEventType type, Transform transform, float delay)
+    {
+        if (DebugEnemy)
+        {
+            Debug.Log("Received Ping");
+        }
+
+        if (type == BellEventType.AirBellEvent)
+        {
+            Frenzy();
+            if (DebugEnemy)
+            {
+                Debug.Log(gameObject.name + " caught Air, frenzy");
+            }
+        }
+        else if (type == BellEventType.FireBellEvent)
+        {
+            Frenzy();
+            if (DebugEnemy)
+            {
+                Debug.Log(gameObject.name + " caught Fire, frenzy");
+            }
+        }
+        else if (type == BellEventType.WaterBellEvent)
+        {
+            Frenzy();
+            if (DebugEnemy)
+            {
+                Debug.Log(gameObject.name + " caught water, frenzy");
+            }
+        }
+        else if (type == BellEventType.EarthBellEvent)
+        {
+            DoDeath();
+            if (DebugEnemy)
+            {
+                Debug.Log(gameObject.name + " caught earth, dying");
+            }
+        }
+    }
 }
